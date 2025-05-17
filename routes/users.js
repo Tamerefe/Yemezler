0df8a27ce7c2241852127713b9b3ce05d8aa850a
Tamer Akipek <tamerakipek@gmail.com>
2025-05-17 13:01:36 +0300

    v1.5-2

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,12 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/menu', (req, res) => {
+    res.render('menu');
+});
+
+router.get('/reservation', (req, res) => {
+    res.render('reservation');
+});
+
 module.exports = router;

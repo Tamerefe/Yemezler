1582af1ccd1aa96166212c289880200e789cf435
Tamer Akipek <tamerakipek@gmail.com>
2025-07-15 16:58:08 +0300

    v1.6
    commit bcc65f4c8f4bc41860ca0fce9c0205e0f23535e8
    Author: Tamer Akipek <tamerakipek@gmail.com>
    Date:   Tue Jul 15 16:08:37 2025 +0300
    
        v1.6
    
    commit 0df8a27ce7c2241852127713b9b3ce05d8aa850a
    Author: Tamer Akipek <tamerakipek@gmail.com>
    Date:   Sat May 17 13:01:36 2025 +0300
    
        v1.5-2
    
    commit c1c0b33388374f38a714aec128dbbd8a7af8dc05
    Author: Tamer Akipek <tamerakipek@gmail.com>
    Date:   Sat May 17 11:54:09 2025 +0300
    
        v1.5-1

--- public/Glass.js
+++ public/Glass.js
@@ -123,7 +123,9 @@ function openCity(evt, foodName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-$('#defaultOpen')[0].click();
+if ($('#defaultOpen')[0]) {
+    $('#defaultOpen')[0].click();
+}
 
 
 // Menu End
@@ -264,13 +266,13 @@ var countdownfunction = setInterval(function () {
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	// Output the result in an element with id="demo"
-	document.getElementById("demo").innerHTML = days + "g " + hours + "s "
-		+ minutes + "d " + seconds + "sn ";
-
-	// If the count down is over, write some text 
-	if (distance < 0) {
-		clearInterval(countdownfunction);
-		document.getElementById("demo").innerHTML = "Bitti";
+	var demoElem = document.getElementById("demo");
+	if (demoElem) {
+		demoElem.innerHTML = days + "g " + hours + "s " + minutes + "d " + seconds + "sn ";
+		if (distance < 0) {
+			clearInterval(countdownfunction);
+			demoElem.innerHTML = "Bitti";
+		}
 	}
 }, 1000);
 
@@ -290,6 +292,7 @@ function showSlides(n) {
 	var i;
 	var slides = document.getElementsByClassName("mySlides");
 	var dots = document.getElementsByClassName("dott");
+	if (!slides.length || !dots.length) return; // Eğer hiç slide veya dot yoksa fonksiyonu bitir
 	if (n > slides.length) { slideIndex = 1 }
 	if (n < 1) { slideIndex = slides.length }
 	for (i = 0; i < slides.length; i++) {
@@ -336,14 +339,24 @@ function password(){
 
 var txt = password();
 let ci = 0;
+var btn = document.getElementById('buttontxt');
 
-$('#buttontxt').bind("click",function typeWriter() {
-	if (ci < txt.length) {
-		document.getElementById("demooo").innerHTML += txt.charAt(ci);
-		ci++;
-		setTimeout(typeWriter, 100);
-	}
-});
+if (btn) {
+    btn.addEventListener('click', function() {
+        btn.disabled = true; // Butonu devre dışı bırak
+        var target = document.getElementById("demooo");
+        target.innerHTML = ""; // Önce temizle
+        var i = 0;
+        function writeChar() {
+            if (i < txt.length) {
+                target.innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(writeChar, 50); // Hızını buradan ayarlayabilirsin
+            }
+        }
+        writeChar();
+    }, { once: true });
+}
 
 
 // Son Güncelleme İle Password Generator Kullanılmıştır 3/26/21
@@ -398,13 +411,15 @@ $('#buttontxt').bind("click",function typeWriter() {
 
   const btn = document.getElementById("generate-btn");
 
-  btn.addEventListener("click", function() {
-    let random = Math.floor(Math.random() * quotes.length);
-    console.log(random);
+  if (btn) {
+    btn.addEventListener("click", function() {
+      let random = Math.floor(Math.random() * quotes.length);
+      console.log(random);
 
-    document.getElementById("quote").textContent = quotes[random].quote;
-    document.querySelector(".author").textContent = quotes[random].author;
-  });
+      document.getElementById("quote").textContent = quotes[random].quote;
+      document.querySelector(".author").textContent = quotes[random].author;
+    });
+  }
 })();
 
 //Random End
@@ -446,7 +461,7 @@ cartBtn.forEach(function(btn){
 
             const item = {};
 
-            item.img = `Urunler${partPath}`;
+            item.img = `/menu/img/${partPath}`;
 
             let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
 
@@ -561,65 +576,80 @@ const counter = document.querySelector('.wrapper > .counter');
 let votes = 83;
 let voted = false;
 
-counter.innerHTML = votes;
+if (counter && voteEl) {
+    counter.innerHTML = votes;
 
-voteEl.addEventListener('click', () => {
-    if (voted) {
-      removeVote();
-    } else {
-      addVote();
-    }  
-});
+    voteEl.addEventListener('click', () => {
+        if (voted) {
+            removeVote();
+        } else {
+            addVote();
+        }
+    });
+}
 
 const addVote = () => {
-  votes = votes + 1;
-  counter.innerHTML = votes;
-  
-  voteEl.style.color = 'green';
-  
-  voted = true;
-}
+    if (counter) {
+        votes++;
+        counter.innerHTML = votes;
+        voted = true;
+    }
+};
 
 const removeVote = () => {
-  votes = votes - 1;
-  counter.innerHTML = votes;
-  
-  voteEl.style.color = '#aaa'
-  
-  voted = false;
-}
+    if (counter) {
+        votes--;
+        counter.innerHTML = votes;
+        voted = false;
+    }
+};
 
 const voteElx = document.querySelector('.wrapperx');
 const counterx = document.querySelector('.wrapperx > .counterx');
-let votesx = 71;
+let votesx = 83;
 let votedx = false;
 
-counterx.innerHTML = votesx;
+if (counterx && voteElx) {
+    counterx.innerHTML = votesx;
 
-voteElx.addEventListener('click', () => {
-    if (votedx) {
-      removeVotex();
-    } else {
-      addVotex();
-    }  
-});
+    voteElx.addEventListener('click', () => {
+        if (votedx) {
+            removeVotex();
+        } else {
+            addVotex();
+        }
+    });
+}
 
 const addVotex = () => {
-  votesx = votesx + 1;
-  counterx.innerHTML = votesx;
-  
-  voteElx.style.color = 'green';
-  
-  votedx = true;
-}
+    if (counterx) {
+        votesx++;
+        counterx.innerHTML = votesx;
+        votedx = true;
+    }
+};
 
 const removeVotex = () => {
-  votesx = votesx - 1;
-  counterx.innerHTML = votesx;
-  
-  voteElx.style.color = '#aaa'
-  
-  votedx = false;
-}
+    if (counterx) {
+        votesx--;
+        counterx.innerHTML = votesx;
+        votedx = false;
+    }
+};
 
 //Vote End
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('reservationForm');
+    if (form) {
+        // Set minimum date to today
+        const today = new Date().toISOString().split('T')[0];
+        document.getElementById('date').min = today;
+        // Form submission
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Teşekkürler! Rezervasyonunuz alınmıştır. Onay e-postası kısa süre içinde gönderilecektir.');
+            form.reset();
+        });
+    }
+});
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('Atess');
+    res.render('home');
 });
 
 router.get('/about', (req, res) => {
-    res.render('Hatess');
+    res.render('about');
+});
+
+router.get('/menu', (req, res) => {
+    res.render('menu');
+});
+
+router.get('/reservation', (req, res) => {
+    res.render('reservation');
 });
 
 module.exports = router;
